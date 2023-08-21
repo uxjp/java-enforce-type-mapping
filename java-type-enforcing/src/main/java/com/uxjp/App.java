238457b1964f80e71dafc25eaa17df305b467fe0
JoaoPedro <joaopedro1172@gmail.com>
2023-08-21 15:12:23 -0300

    Packagable java quickstart

---
+++ java-type-enforcing/src/main/java/com/uxjp/App.java
@@ -0,0 +1,13 @@
+package com.uxjp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
