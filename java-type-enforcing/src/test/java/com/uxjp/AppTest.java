3e5699a2389612f0ee30f84aaf2c99f3c68baa77
JoaoPedro <joaopedro1172@gmail.com>
2023-08-21 16:46:55 -0300

    Automated test for type enforcing

--- java-type-enforcing/src/test/java/com/uxjp/AppTest.java
+++ java-type-enforcing/src/test/java/com/uxjp/AppTest.java
@@ -1,11 +1,22 @@
 package com.uxjp;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
     @Test
     public void testAdd() {
         assertEquals(8, 4 + 4); // This test will pass
+    }
+
+
+    @Test
+    public void testValidateMappingWithValidFields() {
+        GSClass gsInstance = new GSClass();
+        gsInstance.setField1(42);
+        gsInstance.setField2("Hello");
+
+        assertDoesNotThrow(() -> Serializable.validateMapping(gsInstance));
     }
 }
